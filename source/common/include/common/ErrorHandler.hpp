b251706db23faa7dd628ba158238b3935b1139d2
kzueirf12345 <jaba0192837465@gmail.com>
2025-10-08 14:13:23 +0300

    first commit

---
+++ source/common/include/common/ErrorHandler.hpp
@@ -0,0 +1,58 @@
+#ifndef OPTORR_SOURCE_COMMON_INCLUDE_COMMON_ERROR_HANDLER_HPP
+#define OPTORR_SOURCE_COMMON_INCLUDE_COMMON_ERROR_HANDLER_HPP
+
+#include <iostream>
+#include <functional>
+#include <source_location>
+
+#include "Concole.hpp"
+
+namespace Common
+{
+    
+class ErrorHandler {
+    public:
+    template<typename Func, typename... Args>
+    static std::invoke_result_t<Func, Args...> ExecuteWithHandling(
+        const std::source_location location,
+        Func&& func, 
+        Args&&... args) {
+        try {
+            if constexpr (std::is_void_v<std::invoke_result_t<Func, Args...>>){
+                std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+                return 0;
+            } else {
+                return std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+            }
+        } catch (const std::exception& e) {
+            HandleException(e, location);
+            throw; 
+        }
+    }
+
+private:
+
+    static void HandleException(const std::exception& e, 
+                                const std::source_location& location) {
+        std::cerr << RED_FORMAT
+            << "Error at " << location.file_name() 
+            << ":" << location.line() 
+            << " in " << location.function_name() 
+            << YELLOW_FORMAT
+            << "\nMessage: '" 
+            << NORMAL_FORMAT 
+            << e.what() 
+            << YELLOW_FORMAT 
+            << "'"
+            << NORMAL_FORMAT 
+            << "\n";
+    }
+};
+
+#define ERROR_HANDLE(func, ...) \
+    Common::ErrorHandler::ExecuteWithHandling(std::source_location::current(), func, ##__VA_ARGS__)
+
+
+}
+
+#endif /*OPTOR_SOURCE_COMMON_INCLUDE_COMMON_ERROR_HANDLER_HPP*/
